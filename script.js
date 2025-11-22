3a3975174be77abbdd9744661ba15be96d38ff20
OmegaGbenga <gbengadareowolabi@gmail.com>
2025-11-22 11:58:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let container=document.querySelector(".container")
+let colorPallete=document.querySelector(".color-pallete")
+let colorsBtn=document.querySelectorAll(".color")
+let btnContainer=document.querySelectorAll(".btn-container")
+let switchBtn=document.querySelector("button")
+
+
+
+
+colorsBtn.forEach(button=>{
+       button.addEventListener("click", ()=>{
+         let buttonColor= button.id
+
+        container.style.backgroundColor=buttonColor
+       })
+
+      
+})
+
+switchBtn.addEventListener("click", ()=>{
+
+    container.style.backgroundColor='';
+    switchBtn.classList.toggle("active")
+
+    container.classList.toggle("active")
+  
+    
+    
+})
+
+function resetContainer(){
+ 
+}
